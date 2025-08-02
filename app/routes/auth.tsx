bb869a6ee5ad0c614e01964450afb254ed26b7d7
kiratoka <148938667+kiratoka@users.noreply.github.com>
2025-08-02 15:54:31 +0700

    feat(Authentication): Add Auth

---
+++ app/routes/auth.tsx
@@ -0,0 +1,52 @@
+import { useEffect } from "react"
+import { useLocation, useNavigate } from "react-router"
+import { usePuterStore } from "~/lib/puter"
+
+export const meta = () => ([
+    { title: "KiraCV | Auth" },
+    { name: "description", content: "Login menggunakan akunmu" }
+])
+
+const auth = () => {
+    const { isLoading, auth } = usePuterStore()
+    const location = useLocation()
+    const next = location.search.split('next=')[1]
+    const navigate = useNavigate()
+    useEffect(() => {
+        if (auth.isAuthenticated) navigate(next)
+
+    }, [auth.isAuthenticated, next])
+
+    return (
+        <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
+            <div className="gradient-border shadow-lg">
+                <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
+                    <div className="flex flex-col items-center gap-2 text-center">
+                        <h1>Selamat Datang</h1>
+                        <h2>Login untuk memindai CVmu</h2>
+                    </div>
+                    <div>
+                        {isLoading ? (
+                            <button className="auth-button animate-pulse">
+                                <p>Signing you in...</p>
+                            </button>
+                        ) :
+                            <>
+                                {auth.isAuthenticated ?
+                                    <button className="auth-button" onClick={auth.signOut} >
+                                        <p>Log out</p>
+                                    </button>
+                                    :
+                                    <button className="auth-button" onClick={auth.signIn} >
+                                        <p>Log in</p>
+                                    </button>}
+                            </>}
+                    </div>
+                </section>
+
+            </div>
+        </main>
+    )
+}
+
+export default auth
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,48 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
-export function meta({}: Route.MetaArgs) {
+import ResumeCard from "~/components/ResumeCard";
+import { resumes } from "../../constants";
+import { usePuterStore } from "~/lib/puter";
+import { useLocation, useNavigate } from "react-router";
+import { useEffect } from "react";
+
+
+export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Kira CV Analyzer" },
+    { name: "description", content: "Tingkatkan CV Anda dengan AI" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+
+
+  const { auth } = usePuterStore()
+  const navigate = useNavigate()
+  useEffect(() => {
+    if (!auth.isAuthenticated) navigate("auth?next=/")
+
+  }, [auth.isAuthenticated])
+
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Lakukan analisis CV Anda dengan AI</h1>
+        <h2>Dapatkan rekomendasi untuk meningkatkan CV Anda</h2>
+      </div>
+
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+    </section>
+
+
+
+  </main>;
 }
