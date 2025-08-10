a47ab6751d0cdb493987f9d9893c2728f21ed3c7
kiratoka <148938667+kiratoka@users.noreply.github.com>
2025-08-11 06:54:06 +0700

    feat(resume): penambahan details tentang feedback CV

--- app/components/ATS.tsx
+++ app/components/ATS.tsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import { cn } from "~/lib/utils";
 
-const ATS = () => {
+const ATS = ({
+  score,
+  suggestions,
+}: {
+  score: number;
+  suggestions: { type: "good" | "improve"; tip: string }[];
+}) => {
   return (
-    <div>ATS</div>
-  )
-}
+    <div
+      className={cn(
+        "rounded-2xl shadow-md w-full bg-gradient-to-b to-light-white p-8 flex flex-col gap-4",
+        score > 69
+          ? "from-green-100"
+          : score > 49
+            ? "from-yellow-100"
+            : "from-red-100"
+      )}
+    >
+      <div className="flex flex-row gap-4 items-center">
+        <img
+          src={
+            score > 69
+              ? "/icons/ats-good.svg"
+              : score > 49
+                ? "/icons/ats-warning.svg"
+                : "/icons/ats-bad.svg"
+          }
+          alt="ATS"
+          className="w-10 h-10"
+        />
+        <p className="text-2xl font-semibold">Skor ATS - {score}/100</p>
+      </div>
+      <div className="flex flex-col gap-2">
+        <p className="font-medium text-xl">
+          Seberapa bagus CV anda dinilai oleh ATS (Applicant Tracking Systems)?
+        </p>
+        <p className="text-lg text-gray-500">
+          CV Anda telah dipindai seperti yang dilakukan oleh seorang perekrut. Berikut hasilnya:
+        </p>
+        {suggestions.map((suggestion, index) => (
+          <div className="flex flex-row gap-2 items-center" key={index}>
+            <img
+              src={
+                suggestion.type === "good"
+                  ? "/icons/check.svg"
+                  : "/icons/warning.svg"
+              }
+              alt="ATS"
+              className="w-4 h-4"
+            />
+            <p className="text-lg text-gray-500">{suggestion.tip}</p>
+          </div>
+        ))}
+        <p className="text-lg text-gray-500">
+          Ingin Skornya bisa lebih bagus? Tingkatkan dengan mengikuti saran dibawah ini.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default ATS
+export default ATS;
--- app/components/Summary.tsx
+++ app/components/Summary.tsx
@@ -1,9 +1,46 @@
+import ScoreBadge from "./ScoreBadge"
+import ScoreGauge from "./ScoreGauge"
 
+const Category = ({ title, score }: { title: string, score: number }) => {
+    const textColor = score > 70 ? "text-green-600"
+        : score > 49
+            ? "text-yellow-600" : "text-red-600"
 
-const Summary = () => {
-  return (
-    <div>Summary</div>
-  )
+    return (
+        <div className="resume-summary">
+            <div className="category">
+                <div className="flex flex-row gap-2 items-center justify-center">
+                    <p className="text-2xl">{title}</p>
+                    <ScoreBadge score={score}/>
+                </div>
+                <p className="text-2xl">
+                    <span className={textColor}>{score}</span>/100
+                </p>
+            </div>
+
+        </div>
+    )
+}
+
+const Summary = ({ feedback }: { feedback: Feedback }) => {
+    return (
+        <div className="bg-white rounded-2xl shadow-md w-full">
+            <div className="flex flex-row items-center p-4 gap-8">
+                <ScoreGauge feedback={feedback.overallScore} />
+                <div className="flex flex-col gap-2">
+                    <h2 className="text-2xl font-bold">Skor CV Anda</h2>
+                    <p className="text-sm text-gray-500 ">
+                        Skor ini dihitung berdasarkan variabel yang dijabarkan di bawah
+                    </p>
+                </div>
+            </div>
+
+            <Category title="Nada dan gaya bahasa" score={feedback.toneAndStyle.score} />
+            <Category title="Konten" score={feedback.content.score} />
+            <Category title="Struktur" score={feedback.structure.score} />
+            <Category title="Keahlian" score={feedback.skills.score} />
+        </div>
+    )
 }
 
 export default Summary
